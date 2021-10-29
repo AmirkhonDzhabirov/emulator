c23ab726e64d606a9c329e6536882c685ffab459
Amirkhon Dzhabirov <84784096+AmirkhonDzhabirov@users.noreply.github.com>
2021-10-29 17:18:51 +0500

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -1,2 +1,2 @@
-import os
-os.system('cd C://Users//Amirkhon.Jobirov//AppData//Local//Android//Sdk//emulator & emulator -avd Pixel_2_API_28')
+import subprocess
+subprocess.call("cd C://Users//Amirkhon.Jobirov//AppData//Local//Android//Sdk//emulator & emulator -avd Pixel_2_API_28", shell=True)
