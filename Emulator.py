3dc1a779799a4174bbd8b06c54003c257d4d8f0d
Amirkhon Dzhabirov <84784096+AmirkhonDzhabirov@users.noreply.github.com>
2021-10-29 10:04:36 +0500

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -1,7 +1,7 @@
 import os
 import time
 def ept():
-    result = os.system('cd %ANDROID_HOME%/emulator & emulator -list-avds')
+    result = os.system('emulator -list-avds')
     print(result)
 
 ept()
