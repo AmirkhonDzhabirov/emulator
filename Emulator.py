576a3464eb30c25758ad70fef4f43b1d10f06ae1
Amirkhon Dzhabirov <84784096+AmirkhonDzhabirov@users.noreply.github.com>
2021-10-29 10:51:55 +0500

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -1,5 +1,5 @@
 import os
 # result = os.system('cd %ANDROID_HOME%/emulator & emulator -avd Pixel_2_API_28')
-result = os.system('cd C:\Users\Amirkhon.Jobirov\AppData\Local\Android\Sdk\emulator & emulator -avd Pixel_2_API_28')
+result = os.system('emulator -avd Pixel_2_API_28')
 print (result)
 
