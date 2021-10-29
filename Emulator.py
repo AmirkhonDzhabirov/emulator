68bad03228402383df39ee9c723eece0a558b8bd
Amirkhon Dzhabirov <84784096+AmirkhonDzhabirov@users.noreply.github.com>
2021-10-29 18:13:01 +0500

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -1,2 +1,6 @@
 import subprocess
-subprocess.call("cd C://Users//Amirkhon.Jobirov//AppData//Local//Android//Sdk//emulator & emulator -avd Pixel_2_API_28", shell=True)
+# subprocess.call("cd C://Users//Amirkhon.Jobirov//AppData//Local//Android//Sdk//emulator & emulator -avd Pixel_2_API_28", shell=True)
+# # subprocess.Popen("cd %ANDROID_HOME%\emulator")
+
+# subprocess.call('appium.exe', cwd = "C://Users//Amirkhon.Jobirov//AppData//Local//Programs//Appium")
+subprocess.run(["/Users/Amirkhon.Jobirov/AppData/Local/Android/Sdk/emulator/emulator","-avd", "Pixel_2_API_28"])
